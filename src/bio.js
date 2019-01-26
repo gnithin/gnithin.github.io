0beca1ec4c208356b1d167280bb4073455516602
Nithin <nithin.linkin@gmail.com>
2019-01-26 19:31:43 +0530

    [react] Add a basic template layout

--- src/bio.js
+++ src/bio.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
+import './styles/bio.css'
 
 export default class Bio extends Component {
     render() {
         return (
-            <div className="inas">
-                Hi
+            <div className="bio-body-wrapper">
+                <div className="bio-wrapper">
+                    <div className="card">
+                        <div className="card-main-header">
+                            Nithin Gangadharan
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-header">
+                            Current Status
+                        </div>
+                        <div className="card-content">
+                            Senior Sotware Development Engineer, Media.Net, Pune
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-header">
+                            Bio
+                        </div>
+                        <div className="card-content">
+                            - Finished schooling from Kendriya Vidyalaya(CBSE).<br />
+                            - Graduated from Pune University with a Bachelor's Degree in Computer Engineering (B.E 2010-14).<br />
+                            - Working as a software-dev since 2014, at multiple companies in Pune, India.<br />
+                            - Love to work on interesting problems and write (or attempt to) clean code.<br />
+                            - Built server-side solutions in PHP/Python/Go(GCE), forayed into the front-end, building websites with react-js and iOS apps in Objective-C.<br />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-header">
+                            Public Profiles
+                        </div>
+                        <div className="card-content">
+                            - Github<br />
+                            - StackOverflow<br />
+                            - Twitter<br />
+                            - LinkedIn <br />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-header">
+                            Hobbies
+                        </div>
+                        <div className="card-content">
+                            <div className="card-content-header">
+                                Fav Bands
+                            </div>
+                            - Metallica<br />
+                            - Opeth<br />
+                            - Pink Floyd<br />
+                            <br />
+                            <div className="card-content-header">
+                                Fav Books
+                            </div>
+                            - Foundation - Isaac Asimov<br />
+                            - Sapiens: A Brief History of Humankind - Yuval Noah Harari<br />
+                            - The Ancient Art of Stoic Joy -  William B Irvine<br />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
