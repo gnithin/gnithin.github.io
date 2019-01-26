58d8d1eba783f2ea3ca477f1684c021ee6dabcda
Nithin <nithin.linkin@gmail.com>
2019-01-26 19:40:54 +0530

    [build] Move the build dir outside
    [build] Move the build dir outside
    
    [gh-pages] Add the lib
    
    Add the deploy scripts
    
    Move the build to the outside

---
+++ precache-manifest.c759c3e8bdd5878d60107d0b44171e48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80b0d58a314b42674748",
+    "url": "/static/js/main.80b0d58a.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "80b0d58a314b42674748",
+    "url": "/static/css/main.4497c4a7.chunk.css"
+  },
+  {
+    "revision": "73503a27934cb7212de50e64b5bf6e10",
+    "url": "/index.html"
+  }
+];
