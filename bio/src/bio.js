55f2fc615f43a1d6eb0068ec665c9a1a1b896839
Nithin <nithin.linkin@gmail.com>
2019-09-02 10:51:20 -0400

    Update the current status and add the resume

--- bio/src/bio.js
+++ bio/src/bio.js
@@ -11,25 +11,49 @@ export default class Bio extends Component {
                             Nithin Gangadharan
                         </div>
                     </div>
+
                     <div className="card">
                         <div className="card-header">
                             Current Status
                         </div>
                         <div className="card-content">
-                            Senior Sotware Development Engineer, Media.Net, Pune, India
+                            MS in CS, Northeastern University, Boston
                         </div>
                     </div>
+
+                    <div className="card">
+                        <div className="card-header">
+                            Professional Experience
+                        </div>
+                        <div className="card-content">
+                           - Senior Software Development Engineer, Media.Net (Aug 2018 - Jun 2019)<br />
+                           - Software Developer, Media.Net (Dec 2016 - Aug 2018)<br />
+                           - Engineer Software, Ignite Solutions (Apr 2015 - Dec 2016)<br />
+                           - Associate Software Engineer, ACI Worldwide (Jul 2014 - Apr 2015)
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-header">
+                            <a href="./nithin-gangadharan.pdf" target="_blank">Resume (Pdf)</a>
+                        </div>
+                    </div>
+
                     <div className="card">
                         <div className="card-header">
                             Bio
                         </div>
                         <div className="card-content">
-                            - Finished schooling from Kendriya Vidyalaya(CBSE).<br />
+                            - Born in Chennai, Tamil Nadu, India. <br />
+                            - Finished schooling from Kendriya Vidyalaya (CBSE).<br />
                             - Graduated from Pune University with a Bachelor's Degree in Computer Engineering (B.E 2010-14).<br />
-                            - Working as a software-dev since 2014, at multiple companies in Pune, India.<br />
-                            - Love to work on interesting problems and write (or attempt to) clean code (client-side, server-side or anything else :).<br />
+                            - Worked as a software-dev since 2014, at multiple companies in Pune, India.<br />
+                            - Commenced MS in CS, at Northeastern University, from Sept 2019. <br />
+                            - Loves to work on interesting problems and write (or attempt to) clean code.<br />
+                            - Interested on working across the stack; client-side, server-side or anything else :) <br />
                         </div>
                     </div>
+
                     <div className="card">
                         <div className="card-header">
                             Programming Languages/Frameworks I like
@@ -41,6 +65,7 @@ export default class Bio extends Component {
                             - Objective-C - iOS <br />
                         </div>
                     </div>
+
                     <div className="card">
                         <div className="card-header">
                             Public Profiles
@@ -53,11 +78,16 @@ export default class Bio extends Component {
                             - <a href="mailto:nithin.linkin@gmail.com" target="_blank" rel="noopener noreferrer"> Email </a><br />
                         </div>
                     </div>
+
                     <div className="card">
                         <div className="card-header">
                             Hobbies
                         </div>
                         <div className="card-content">
+                            - Likes to go on a run.<br />
+                            - Loves Metallica.<br />
+                            - Enjoys reading a novel, occasionally. <br />
+                            <br />
                             <div className="card-content-header">
                                 Fav Bands
                             </div>
